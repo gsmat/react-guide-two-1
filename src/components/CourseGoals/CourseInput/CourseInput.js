e56f0ac3052cb362e6aef78721ed72529eec779f
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-01 12:38:07 +0400

    add module and use

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
 import Button from "../../UI/Button/Button";
-import styled from "styled-components";
+import styles from './CourseInput.module.css';
+// import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color : ${props => props.invalid ? 'red' : 'black' }
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color : ${props => props.invalid ? 'red' : 'black' }
+//   }
 
-  @media(min-width:700px){
-    width:auto;
-  }
+//   @media(min-width:700px){
+//     width:auto;
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")}};
-    background : ${props=>(props.invalid ? 'red' : 'black')}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")}};
+//     background : ${props=>(props.invalid ? 'red' : 'black')}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-`;
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -55,25 +56,47 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      {/* <FormControl className={!isValid && 'invalid'}> */}
-      <FormControl invalid={!isValid}>
-        {/* <label style={{ color: !isValid ? "red" : "" }}>Course Goal</label> */}
-        <label>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? "red" : "",
-            backgroundColor: !isValid ? "salmon" : "white",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-        {!isValid && error}
-      </FormControl>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+//   return (
+//     <form onSubmit={formSubmitHandler}>
+//       {/* <FormControl className={!isValid && 'invalid'}> */}
+//       <FormControl invalid={!isValid}>
+//         {/* <label style={{ color: !isValid ? "red" : "" }}>Course Goal</label> */}
+//         <label>Course Goal</label>
+//         <input
+//           style={{
+//             borderColor: !isValid ? "red" : "",
+//             backgroundColor: !isValid ? "salmon" : "white",
+//           }}
+//           type="text"
+//           onChange={goalInputChangeHandler}
+//         />
+//         {!isValid && error}
+//       </FormControl>
+//       <Button type="submit">Add Goal</Button>
+//     </form>
+//   );
+// };
+
+return (
+  <form onSubmit={formSubmitHandler}>
+    {/* <FormControl className={!isValid && 'invalid'}> */}
+    <div className={`${styles.['form-control']} ${!isValid && styles.invalid}`}>
+      {/* <label style={{ color: !isValid ? "red" : "" }}>Course Goal</label> */}
+      <label>Course Goal</label>
+      <input
+        style={{
+          borderColor: !isValid ? "red" : "",
+          backgroundColor: !isValid ? "salmon" : "white",
+        }}
+        type="text"
+        onChange={goalInputChangeHandler}
+      />
+      {!isValid && error}
+    </div>
+    <Button type="submit">Add Goal</Button>
+  </form>
+);
 };
+
 
 export default CourseInput;
